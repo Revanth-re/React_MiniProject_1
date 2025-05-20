b8b485bf631d15d985553b001c5511512940a005
Revanth <revanthrevi131@gmail.com>
2025-05-20 12:13:45 +0530

    df

--- src/Components/NavbarSection/Navbar.jsx
+++ src/Components/NavbarSection/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import {Link} from "react-router-dom"
-import {Navbar,Container,Nav} from "react-bootstrap"
+import {Navbar,Container,Nav,Badge} from "react-bootstrap"
 import { TiShoppingCart } from "react-icons/ti";
 import { FaHeart } from "react-icons/fa";
 
 
+
 import "./Navbar.css"
 const NavbarSection = () => {
+    const cartItems=JSON.parse(localStorage.getItem("CartItems"))
+    const favItems=JSON.parse(localStorage.getItem("FavItems"))
   return (
     <div>
       <Navbar expand="lg" className="bg-body-tertiary">
@@ -20,8 +23,8 @@ const NavbarSection = () => {
   navbarScroll
 >
   <Link to="/products" className="nav-link-custom">Products</Link>
-  <Link to="/FavSection" className="nav-link-custom"><FaHeart></FaHeart></Link>
-  <Link to="/Cart" className="nav-link-custom"><TiShoppingCart/></Link>
+  <Link to="/FavSection" className="nav-link-custom"><FaHeart/>  <Badge bg="secondary">{favItems.length}</Badge></Link>
+  <Link to="/Cart" className="nav-link-custom"><TiShoppingCart/> <Badge bg="secondary">{cartItems.length}</Badge></Link>
 </Nav>
 
         </Navbar.Collapse>
